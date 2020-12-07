b3449b1bfa156af34a586aaceac709871e850202
rojayzhuang <651577372@qq.com>
2020-12-07 17:51:56 +0800

    项目初始化搭建

---
+++ src/main/java/com/rojay/Site5gApplication.java
@@ -0,0 +1,16 @@
+package com.rojay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ */
+@SpringBootApplication
+public class Site5gApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Site5gApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rojay/site5g/Site5gApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojay.site5g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site5gApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
